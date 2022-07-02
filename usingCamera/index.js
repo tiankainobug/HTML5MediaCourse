7c94cee79a914fe066313b8d449d6f4524f3e679
tiank <tiank@deepblueai.com>
2022-07-03 00:20:08 +0800

    拍照功能

--- usingCamera/index.js
+++ usingCamera/index.js
@@ -1,4 +1,15 @@
-window.onload = async function (){
-    let stream = await navigator.mediaDevices.getUserMedia({video:true,audio:false})
-    document.querySelector('video').srcObject = stream;
-}
+new Vue({
+    el:'#vueapp',
+    mounted(){
+        this.initVideoDevice()
+    },
+    methods:{
+        async initVideoDevice(){
+            this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia({video:true,audio:false})
+            this._context2d = this.$refs.canvas.getContext('2d')
+        },
+        takePhoto(){
+            this._context2d.drawImage(this.$refs.video,0,0,400,300)
+        }
+    }
+})
